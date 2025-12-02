c37dcd56ca2434236485985e43aea337dada0ccc
Tanmay Pandey <tanmaypanday.lko@gmail.com>
2025-12-02 09:41:29 +0530

    Created Blog Page and updated Shop page

--- script.js
+++ script.js
@@ -21,17 +21,35 @@ if (close) {
 var MainImg = document.getElementById("MainImg");
 var smallimg = document.getElementsByClassName("small-img");
 
-if (MainImg) { // Check if MainImg exists (only on sproduct.html)
-    smallimg[0].onclick = function() {
-        MainImg.src = smallimg[0].src;
+// 1. Logic to change main image when clicking small images (Gallery)
+if (MainImg) {
+    if(smallimg.length > 0) {
+        smallimg[0].onclick = function() {
+            MainImg.src = smallimg[0].src;
+        }
+        smallimg[1].onclick = function() {
+            MainImg.src = smallimg[1].src;
+        }
+        smallimg[2].onclick = function() {
+            MainImg.src = smallimg[2].src;
+        }
+        smallimg[3].onclick = function() {
+            MainImg.src = smallimg[3].src;
+        }
     }
-    smallimg[1].onclick = function() {
-        MainImg.src = smallimg[1].src;
-    }
-    smallimg[2].onclick = function() {
-        MainImg.src = smallimg[2].src;
-    }
-    smallimg[3].onclick = function() {
-        MainImg.src = smallimg[3].src;
+    
+    // 2. Logic to load the correct product image from the URL parameter
+    // This allows sproduct.html to work for ANY product
+    const urlParams = new URLSearchParams(window.location.search);
+    const imageParam = urlParams.get('image');
+
+    if (imageParam) {
+        MainImg.src = imageParam;
+        
+        // Optional: Reset small gallery images to match the main image 
+        // (Since we don't have separate gallery images for every product in this tutorial)
+        for(let i=0; i<smallimg.length; i++){
+            smallimg[i].src = imageParam;
+        }
     }
 }
